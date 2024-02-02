5f636f4bd5fe1ffa6347e88cbb921ab4113d9650
Nicolegoncalo <nicolegoncalo041@gmail.com>
2024-02-02 16:50:22 -0300

    01
    primeira vez com php

---
+++ infor.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+<?php
+
+$nome ="jussara";
+$idade = 16;
+$peso = 52.5;
+$doacao = false;
+
+echo "meu nome é " .$nome. "<br>";
+echo'idade ' .$idade. "<br>";
+echo 'peso ' .$peso. "<br>";
+echo 'doacao' .$doacao. "<br>";
+
+$a=7;
+$b=9;
+$resultado=$a+$b. "<br>";
+
+var_dump($b);
+var_dump($peso);
+var_dump($a)
+
+?>
+
+</body>
+</html>
